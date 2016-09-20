2c4e8c904f63e5a0b3a78116fd225d3ce9d7872f
zql <zhangql@civaonline.cn>
2016-09-20 17:22:22 +0800

    Initial commit

---
+++ app/src/main/java/com/hw/MainActivity.java
@@ -0,0 +1,10 @@
+package com.hw;
+
+import com.hw.baselib.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentView(R.layout.activity_main);
+    }
+}
